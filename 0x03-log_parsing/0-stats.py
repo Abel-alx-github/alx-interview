379e04e308d3d3ebe421f5c92f77c05b0e272a18
abel-alx-github <abeltibeso323@gmail.com>
2024-06-20 11:54:07 +0300

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,40 +1,35 @@
 #!/usr/bin/python3
 """ a module contains a script that parse input from stdin"""
 
-
 if __name__ == '__main__':
     import sys
-
-    dic = {}
-    list_status = [200, 301, 400, 401, 403, 404, 405, 500]
     file_size = 0
     count = 0
+    status_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    dic_stats = {k: 0 for k in status_codes}
 
-    def print_metric(dic: dict, file_size: int) -> None:
+    def print_metric(dic_stats: dict, file_size: int) -> None:
         print("File size: {:d}".format(file_size))
-        for k in list_status:
-            if k in dic.keys():
-                print("{}: {}".format(k, dic[k]))
-
+        for k, v in sorted(dic_stats.items()):
+            if v:
+                print("{}: {}".format(k, v))
     try:
         for line in sys.stdin:
             count += 1
             data = line.split()
             try:
-                status = int(data[-2])
-                size = int(data[-1])
-                if status in list_status:
-                    if status in dic.keys():
-                        dic[status] += 1
-                        file_size += size
-                    else:
-                        dic[status] = 1
-                        file_size += size
+                stat_code = data[-2]
+                if stat_code in dic_stats:
+                    dic_stats[stat_code] += 1
+            except BaseException:
+                pass
+            try:
+                file_size += int(data[-1])
             except BaseException:
                 pass
             if count % 10 == 0:
-                print_metric(dic, file_size)
-        print_metric(dic, file_size)
+                print_metric(dic_stats, file_size)
+        print_metric(dic_stats, file_size)
     except KeyboardInterrupt:
-        print_metric(dic, file_size)
+        print_metric(dic_stats, file_size)
         raise
