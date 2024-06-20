9ad74f0aa6e49d75d3b72da3c28cfb500558b7f9
abel-alx-github <abeltibeso323@gmail.com>
2024-06-20 11:23:58 +0300

    log parsing interview question

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,18 +1,12 @@
 #!/usr/bin/python3
 """ a module contains a script that parse input from stdin"""
 
-import sys
-import re
-
 
 if __name__ == '__main__':
-    pattern_str = (
-        r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\s-\s\[\d{4}-\d{2}-\d{2}\s"
-        r"\d{2}:\d{2}:\d{2}\.\d+\]\s\"GET /projects/260 HTTP/1.1\"\s\d{3}\s\d+"
-    )
+    import sys
 
     dic = {}
-    list_status = [500, 405, 404, 403, 401, 400, 301, 200]
+    list_status = [200, 301, 400, 401, 403, 404, 405, 500]
     file_size = 0
     count = 0
 
@@ -25,12 +19,10 @@ if __name__ == '__main__':
     try:
         for line in sys.stdin:
             count += 1
-            if not line:
-                break
-            found = re.search(pattern_str, line)
-            if found:
-                size = int(list(found.string.split())[-1])
-                status = int(list(found.string.split())[-2])
+            data = line.split()
+            try:
+                status = int(data[-2])
+                size = int(data[-1])
                 if status in list_status:
                     if status in dic.keys():
                         dic[status] += 1
@@ -38,8 +30,11 @@ if __name__ == '__main__':
                     else:
                         dic[status] = 1
                         file_size += size
+            except BaseException:
+                pass
             if count % 10 == 0:
                 print_metric(dic, file_size)
+        print_metric(dic, file_size)
     except KeyboardInterrupt:
         print_metric(dic, file_size)
         raise
