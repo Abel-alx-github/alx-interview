69a44803afa78f0b19d2f009d614529dd284e575
abel-alx-github <abeltibeso323@gmail.com>
2024-06-28 00:28:26 +0300

    utf-8 validator

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" utf-8 validation """
+
+
+def validUTF8(data):
+    """
+    data: List[int]
+    return type: bool
+    """
+    ones = 0
+    for num in data:
+        num = num & 0xFF
+        if num & 0x80:
+            if ones == 0:
+                ones += 1
+                if num & 0xC0 == 0xC0:
+                    ones = 2
+                elif num & 0xE0 == 0xE0:
+                    ones = 3
+                elif num & 0xF0 == 0xF0:
+                    ones = 4
+                else:
+                    return False
+            elif ones > 0:
+                if not (num & 0x80 and not (num & 0x40)):
+                    return False
+                ones -= 1
+        else:
+            if ones > 0:
+                return False
+            ones = 0
+    return ones == 0
