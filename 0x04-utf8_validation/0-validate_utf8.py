3c36974af50ab35343e9f56191bf79feef3897c4
abel-alx-github <abeltibeso323@gmail.com>
2024-06-28 01:28:36 +0300

    utf-8 validator

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,19 +6,11 @@ def validUTF8(data):
     ones = 0
     for num in data:
         if ones == 0:
-            if num >> 7:
-                return False
-            elif num >> 5 == 0b110:
-                if len(data) < 2:
-                    return False
+            if num >> 5 == 0b110:
                 ones = 1
             elif num >> 4 == 0b1110:
-                if len(data) < 3:
-                    return False
                 ones = 2
             elif num >> 3 == 0b11110:
-                if len(data) < 4:
-                    return False
                 ones = 3
             elif num >> 7:
                 return False
