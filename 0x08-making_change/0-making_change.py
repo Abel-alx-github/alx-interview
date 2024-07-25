e5cc25f42a5a1b930c60eb041155413ceb868b72
abel-alx-github <abeltibeso323@gmail.com>
2024-07-25 17:35:58 +0300

    Making Change Problem

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,13 +7,14 @@ def make_change(coins, total):
     to meet a given amount total"""
     if total <= 0:
         return 0
-    sum = 0
+
+    sum_used = 0
     usedcoins = 0
     coins = sorted(coins, reverse=True)
     for coin in coins:
-        r = (total - sum) // coin
-        sum += r * coin
-        usedcoins += r
-        if sum == total:
+        left = (total - sum_used)
+        sum_used += left * coin
+        usedcoins += left
+        if sum_used == total:
             return usedcoins
     return -1
