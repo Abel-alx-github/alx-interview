1365e86cf61184d1e825e421df599443eaa3acda
abel-alx-github <abeltibeso323@gmail.com>
2024-07-25 19:14:17 +0300

    making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,14 +5,15 @@
 def makeChange(coins, total):
     """Determines the fewest number of coins needed \
     to meet a given amount total"""
+   
     if total <= 0:
         return 0
 
     sum_used = 0
     usedcoins = 0
-    coins = sorted(coins, reverse=True)
-    for coin in coins:
-        left = (total - sum_used)
+    sorted_coins = sorted(coins, reverse=True)
+    for coin in sorted_coins:
+        left = (total - sum_used) // coin
         sum_used += left * coin
         usedcoins += left
         if sum_used == total:
